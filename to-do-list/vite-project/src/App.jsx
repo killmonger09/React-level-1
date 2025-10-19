d97157b2d7135880da6d4b1e771aab952b52efac
User Name <ppratya21@gmail.com>
2025-10-19 18:38:50 +0530

    simple to do list app

---
+++ to-do-list/vite-project/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+
+  const handleDelete = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  }
+
+  const handleAdd = () => {
+          if (inputValue.trim() === "") return;
+          const newTodo = { id: Date.now(), text: inputValue };
+          setTodos([...todos, newTodo]);
+          setInputValue("");
+        }
+
+  return (
+    <>
+      <p>TO DO - List</p>
+
+      <input
+        placeholder="write something"
+        className="task-name"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+
+      <button
+        onClick={handleAdd}
+        > Add
+      </button>
+
+      <div>
+        {todos.map((todo) => (
+          <p key={todo.id}>
+            <span>{todo.text}</span>
+          <button
+          onClick={() => handleDelete(todo.id)}
+          >Delete</button></p>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
