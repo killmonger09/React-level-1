499838c1bbbad8d451bf05fb200ef630c8bca9bb
User Name <ppratya21@gmail.com>
2025-10-27 23:31:11 +0530

    new to do app

---
+++ To-do-App/vite-project/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const handleTodo = () => {
+    if ( inputValue.trim() === '') return;
+    const newTodo = {id: Date.now() , text: inputValue}
+    setTodos([...todos, newTodo ]);
+    setInputValue("")
+  };
+
+  const handleDelete = () => {
+    todos.filter(todo.id !== id)
+  }
+
+  return (
+    <>
+     <input 
+     placeholder='write something'
+      value={inputValue}
+      
+      onChange={(e) =>setInputValue(e.target.value)}
+     />
+     <button
+     onClick={handleTodo}
+     >Add</button>
+      <div>
+        {todos.map(todo => {
+          <p key= {todo.id}>
+          <span>{todo.text}</span>
+          <button
+        onClick={handleDelete}
+        >Delete</button>
+          </p>
+          
+        })}
+        
+        </div>       
+    </>
+  )
+}
+
+export default App
